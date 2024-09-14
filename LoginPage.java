858650f8eff49b263df41832db0acc7d6c1d9bfc
SarahAshour <sarahashour95@gmail.com>
2024-09-14 15:19:28 +0300

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -28,3 +28,22 @@ public class LoginPage {
         driver.findElement(loginButton).click();
     }
 }
+
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void loginTest() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.enterUsername("nxttester");  // Replace with valid username
+        loginPage.enterPassword("nxt#qA@23");  // Replace with valid password
+        loginPage.clickLogin();
+
+    }
+}
